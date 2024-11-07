efa83aed2189393da8653edcc1c73fffd979149a
mundane <799699348@qq.com>
2024-11-07 23:57:04 +0800

    修改了一点响应式布局

--- components/SiteSection.tsx
+++ components/SiteSection.tsx
@@ -21,7 +21,7 @@ export default function SiteSection({ title, sites }: SiteSectionProps) {
       </h4>
 
       {/* 网站卡片网格 */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
         {/* 网站卡片... */}
         {sites.map((site, index) => (
           <SiteCard
