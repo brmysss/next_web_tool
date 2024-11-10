ce2aec6080fc37f14e59d6d576155b5cce7ee434
mundane <799699348@qq.com>
2024-11-10 20:28:58 +0800

    暂存一波

--- components/SearchSection.tsx
+++ components/SearchSection.tsx
@@ -12,35 +12,54 @@ export default function SearchSection() {
 
   return (
     <div className="flex justify-center items-center py-20 bg-[url('/images/bg-dna.jpg')]">
-      <div className="s-search">
-        <div id="search" className="s-search mx-auto">
-          <div className="search-wrapper">
-            {/* 搜索类型选择 */}
-            <div className="search-content">
-              <div className="search-input">
-                <form onSubmit={handleSubmit}>
-                  <input
-                    className="rounded-full w-[800px] h-[50px] text-xl px-5 py-2 outline-none"
-                    type="text"
-                    placeholder="输入关键字搜索"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                  />
-                </form>
-              </div>
-              <div className="search-btn">
-                <button type="submit">
-                  <i className="iconfont icon-search" />
-                </button>
-              </div>
-            </div>
-          </div>
-
-          {/* 搜索引擎选择 */}
-          <div className="search-engine">
-            <div className="search-engine-list">{/* 搜索引擎选项... */}</div>
-          </div>
+      <div className="flex flex-col items-center">
+        <div className="flex text-green-500/50 w-[420px]">
+          <label
+            htmlFor="search-text"
+            className="flex-1 text-center py-2 px-4 hover:text-green-500"
+          >
+            <span>常用</span>
+          </label>
+          <label
+            htmlFor="search-engine"
+            className="flex-1 text-center py-2 px-4 hover:text-green-500"
+          >
+            <span>搜索</span>
+          </label>
+          <label
+            htmlFor="search-text"
+            className="flex-1 text-center py-2 px-4 hover:text-green-500"
+          >
+            <span>工具</span>
+          </label>
+          <label
+            htmlFor="search-engine"
+            className="flex-1 text-center py-2 px-4 hover:text-green-500"
+          >
+            <span>社区</span>
+          </label>
+          <label
+            htmlFor="search-engine"
+            className="flex-1 text-center py-2 px-4 hover:text-green-500"
+          >
+            <span>生活</span>
+          </label>
+          <label
+            htmlFor="search-engine"
+            className="flex-1 text-center py-2 px-4 hover:text-green-500"
+          >
+            <span>求职</span>
+          </label>
         </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="rounded-full w-[800px] h-[50px] text-xl px-5 py-2 outline-none"
+            type="text"
+            placeholder="输入关键字搜索"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </form>
       </div>
     </div>
   );
