b9c1ca9b5d6da0c4bf679edb7927753115377f8d
mundane <799699348@qq.com>
2024-11-10 17:53:34 +0800

    搜索框，暂存一波

--- components/SearchSection.tsx
+++ components/SearchSection.tsx
@@ -5,22 +5,28 @@ import { useState } from "react";
 export default function SearchSection() {
   const [searchText, setSearchText] = useState("");
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(searchText);
+  };
+
   return (
-    <div className="flex justify-center items-center py-4 bg-[url('/images/bg-dna.jpg')]">
+    <div className="flex justify-center items-center py-20 bg-[url('/images/bg-dna.jpg')]">
       <div className="s-search">
         <div id="search" className="s-search mx-auto">
           <div className="search-wrapper">
             {/* 搜索类型选择 */}
             <div className="search-content">
               <div className="search-input">
-                <input
-                  type="text"
-                  id="search-text"
-                  className="form-control search-key"
-                  placeholder="输入关键字搜索"
-                  value={searchText}
-                  onChange={(e) => setSearchText(e.target.value)}
-                />
+                <form onSubmit={handleSubmit}>
+                  <input
+                    className="rounded-full w-[800px] h-[50px] text-xl px-5 py-2 outline-none"
+                    type="text"
+                    placeholder="输入关键字搜索"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                  />
+                </form>
               </div>
               <div className="search-btn">
                 <button type="submit">
