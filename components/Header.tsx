aff9428cac651ead7d6a7c55e5e2e6372a734e72
mundane <799699348@qq.com>
2024-11-13 14:43:29 +0800

    增加了首页和关于两个按钮

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { BookText, Home, Info, Menu } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Header() {
@@ -16,13 +16,21 @@ export default function Header() {
   return (
     <header
       className={`fixed top-0 left-[170px] h-[74px] right-0 z-50 transition-colors duration-300 ${
-        isScrolled ? "bg-white/70 backdrop-blur-sm text-gray-500" : "text-white"
+        isScrolled ? "bg-white/70 backdrop-blur-sm text-gray-700" : "text-white"
       }`}
     >
-      <div className="flex items-center justify-between h-16">
+      <div className="flex items-center h-16 gap-4">
         {/* 菜单图标 */}
         <button className="p-2">
-          <Menu />
+          <Menu className="w-6 h-6" />
+        </button>
+        <button className="p-2 flex items-center">
+          <Home className="w-6 h-6" />
+          <span className="text-sm">首页</span>
+        </button>
+        <button className="p-2 flex items-center">
+          <BookText className="w-6 h-6" />
+          <span className="text-sm">关于</span>
         </button>
       </div>
     </header>
