6f52a3e227c4a7c7981e3e4057d6c0fdeef1a4ec
mundane <799699348@qq.com>
2024-11-13 19:52:11 +0800

    更新readme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,18 +2,17 @@ const Footer = () => {
   return (
     <div className="text-center py-4 text-xs">
       <p>
-        本站内容源自互联网，如有内容侵犯了你的权益，请联系删除相关内容，联系邮箱：kefu308@gmail.com
+        本站内容源自互联网，如有内容侵犯了你的权益，请联系删除相关内容，联系邮箱：zfyoung799699@gmail.com
       </p>
       <p>
         © 2021 - {new Date().getFullYear()} By{" "}
         <a
-          href="https://webtool.fun"
-          title="Web_tool"
+          href="https://github.com/mundane799699/next_web_tool"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500 hover:text-red-500"
         >
-          Web_tool
+          next_web_tool
         </a>{" "}
         |{" "}
         <a
