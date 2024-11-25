06eb06750c48198802901622db4dffd543602752
mundane <799699348@qq.com>
2024-11-25 18:40:26 +0800

    暂存一波

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,11 @@ import { useSettings } from "@/hooks/use-settings";
 import FriendLinkSection from "@/components/FriendLinkSection";
 import Footer from "@/components/Footer";
 import { useMediaQuery } from "usehooks-ts";
-import { Moon, MoonStar, Search, Sun } from "lucide-react";
+import { Moon, MoonStar, Search, Sun, X } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export default function Home() {
+  const [showSearchDialog, setShowSearchDialog] = useState(false);
   const { isCollapsed, isMobileMenuOpen, setIsCollapsed, setIsMobileMenuOpen } =
     useSettings();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -35,6 +36,35 @@ export default function Home() {
   useEffect(() => {
     setIsCollapsed(false);
   }, [isMobile]);
+
+  const SearchDialog: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
+    isOpen,
+    onClose,
+  }) => {
+    return (
+      <div
+        className={`fixed inset-0 z-[9999] flex items-center justify-center bg-black/50 backdrop-blur-sm animate-in fade-in duration-300 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <div className="w-[480px] max-w-[90vw] rounded-lg bg-white animate-in slide-in-from-top duration-300">
+          <div className="flex items-center justify-between border-b px-6 py-4">
+            <button
+              onClick={onClose}
+              className="rounded-full p-1 hover:bg-gray-100"
+            >
+              <X className="h-5 w-5" />
+            </button>
+          </div>
+          <div className="p-6">
+            <div>
+              <h1>分享书摘</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="min-h-screen flex overflow-x-hidden">
@@ -69,7 +99,10 @@ export default function Home() {
         />
         {/* 搜索按钮和夜间模式切换按钮 */}
         <div className="flex flex-col gap-2 fixed bottom-5 right-5 text-black dark:text-white">
-          <button className="bg-white dark:bg-[#2c2e2f]  p-3 rounded-full">
+          <button
+            className="bg-white dark:bg-[#2c2e2f]  p-3 rounded-full"
+            onClick={() => setShowSearchDialog(true)}
+          >
             <Search className="w-4 h-4" />
           </button>
           <button
@@ -84,6 +117,12 @@ export default function Home() {
           </button>
         </div>
       </div>
+
+      {/* 弹窗 */}
+      <SearchDialog
+        isOpen={showSearchDialog}
+        onClose={() => setShowSearchDialog(false)}
+      />
     </div>
   );
 }
